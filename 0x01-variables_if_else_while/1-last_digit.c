23ce7a2e847ba2fec2ebd90680fbbc2b2864d86a
amusa yusuf <gsconglomerates@gmail.com>
2022-07-04 20:25:37 -0700

    fixed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,15 +10,17 @@
 int main(void)
 {
 int n;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 5)
-printf("%d and is greater than 5\n", n);
+printf("and is greater than 5");
 if (n == 0)
-printf("%d and is 0\n", n);
+printf("and is 0");
 if (n < 6 && n != 0)
 {
-printf("%d and is less than 6 and not 0\n", n);
+printf("and is less than 6 and not 0);
 }
+printf("\n");
 return (0);
 }
