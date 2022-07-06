c759aee9c80b70adacbd4dc88dc8b5eaaa888589
amusa yusuf <gsconglomerates@gmail.com>
2022-07-06 15:58:21 -0700

    islower

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Adds two integers.
+ * Return: The result of the multiplication.
+ */
+
+void print_alphabet_x10(void);
+{
+	int j;
+	int x;
+
+	for (x = 0; x<10; x++)
+	{
+		for (j = 'a'; j <= 'z'; j++
+		{
+			_putchar(j)
+		}
+
+		_putchar('\n')\
+	}
+}
