c4edc0ff1ee6e40d3105b469fa0c4c905f500a6a
amusa yusuf <gsconglomerates@gmail.com>
2022-07-06 14:53:51 -0700

    purchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "Hamzology followed by a new line.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('a');
+	_putchar('m');
+	_putchar('z');
+	_putchar('o');
+	_putchar('l');
+	_putchar('o');
+	_putchar('g');
+	_putchar('y');
+	_putchar('\n');
+	return (0);
+}
