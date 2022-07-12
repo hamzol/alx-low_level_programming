76ae36970b263b762f9698c3c1726f643f46d4ec
amusa yusuf <gsconglomerates@gmail.com>
2022-07-12 13:31:50 -0700

    98 battery

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int *n;
+
+    n = 98;
+    putchar("n=%d\n", n);
+    putchar("n=%d\n", n);
+    return (0);
+}
